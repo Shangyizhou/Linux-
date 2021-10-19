7e868b87bb18692d376e54205d119ef18d0c26b6
SYZ <71711056+Shangyizhou@users.noreply.github.com>
2021-10-19 14:48:39 +0800

    Update man 7 signal.c

--- 网络编程/信号/man 7 signal.c
+++ 网络编程/信号/man 7 signal.c
@@ -1,4 +1,5 @@
 ```C
+
 可通过man 7 signal查看帮助文档获取。也可查看/usr/src/linux-headers-3.16.0-30/arch/s390/include/uapi/asm/signal.h
 
 Signal      Value   Action  Comment
@@ -46,4 +47,5 @@ The signals SIGKILL and SIGSTOP cannot be caught, blocked, or ignored.
 在标准信号中，有一些信号是有三个“Value”，第一个值通常对alpha和sparc架构有效，中间值针对x86、arm和其他架构，最后一个应用于mips架构。一个‘-’表示在对应架构上尚未定义该信号。
 
 不同的操作系统定义了不同的系统信号。因此有些信号出现在Unix系统内，也出现在Linux中，而有的信号出现在FreeBSD或Mac OS中却没有出现在Linux下。这里我们只研究Linux系统中的信号。
+
 ```
